755dfcbcf422e333a4ed8d1f172f4cbf819f4ab1
Ilias Mentzelos <iliasmentz@gmail.com>
2018-09-15 18:03:40 +0300

    Bootstrap package fixes
    Fix the modal functionality.

--- src/main/client/src/app/app.module.ts
+++ src/main/client/src/app/app.module.ts
@@ -21,7 +21,7 @@ import {NewsfeedComponent} from './newsfeed/newsfeed.component';
 import {BasicInfoComponent} from './profile/basic-info/basic-info.component';
 import {ExperienceComponent} from './profile/experience/experience.component';
 import {EducationComponent} from './profile/education/education.component';
-import {BsDatepickerModule, ModalModule, ProgressbarModule} from "ngx-bootstrap";
+import {BsDatepickerModule, BsModalRef, ModalModule, ProgressbarModule} from "ngx-bootstrap";
 import {EducationService} from "./profile/education/education.service";
 import {MomentModule} from "angular2-moment";
 import {SkillsComponent} from "./profile/skills/skills.component";
@@ -61,12 +61,12 @@ import {BasicInfoModalComponent} from './profile/basic-info/basic-info-modal/bas
     ProgressbarModule.forRoot(),
     ModalModule.forRoot()
   ],
-  entryComponents:[
+  entryComponents: [
     BasicInfoModalComponent
   ],
   providers: [
     Globals, AuthGuard, AuthService, RepoService, UserService,
-    EducationService, ExperienceService, SkillService,
+    EducationService, ExperienceService, SkillService, BsModalRef,
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptor,
@@ -76,3 +76,4 @@ import {BasicInfoModalComponent} from './profile/basic-info/basic-info-modal/bas
 })
 export class AppModule {
 }
+
