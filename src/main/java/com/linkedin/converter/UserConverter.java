7e65212bdac80bee26c683dbb256acd530c814d1
MARINOSAG <sdi1400002@di.uoa.gr>
2018-09-30 17:33:41 +0300

    Current Experience to UserDto also and Experience.csv correct titles

--- src/main/java/com/linkedin/converter/UserConverter.java
+++ src/main/java/com/linkedin/converter/UserConverter.java
@@ -17,74 +17,78 @@ import java.util.List;
 @Component
 public class UserConverter {
 
-	private final UserRepository userRepository;
-	private final ExperienceService experienceService;
-	private final ExperienceRepository experienceRepository;
-	@Autowired
-	public UserConverter(UserRepository userRepository, ExperienceService experienceService, ExperienceRepository experienceRepository) {
-		this.userRepository = userRepository;
-	  this.experienceService = experienceService;
-	  this.experienceRepository = experienceRepository;
+  private final UserRepository userRepository;
+  private final ExperienceService experienceService;
+  private final ExperienceRepository experienceRepository;
+
+  @Autowired
+  public UserConverter(UserRepository userRepository, ExperienceService experienceService, ExperienceRepository experienceRepository) {
+	this.userRepository = userRepository;
+	this.experienceService = experienceService;
+	this.experienceRepository = experienceRepository;
+  }
+
+
+  public UserDto toUserDto(User user, Integer role) {
+	UserDto userDto = new UserDto();
+	userDto.setEmail(user.getEmail());
+	userDto.setUserId(user.getId());
+	userDto.setName(user.getName());
+	userDto.setSurname(user.getSurname());
+	userDto.setDisplayName(user.getName() + ' ' + user.getSurname());
+	userDto.setUsername(user.getUsername());
+	userDto.setBirthdate(user.getBirthdate());
+	userDto.setAddress(user.getAddress());
+	userDto.setPhoneNumber(user.getPhoneNumber());
+	userDto.setImagePath(getUserPhotoFullUrl(user));
+	userDto.setDateCreated(user.getDateCreated());
+	userDto.setRole(role);
+
+	Experience experience = experienceRepository.findAllByUserIdOrderByStartDateDesc(user.getId()).stream().findFirst().orElse(null);
+	if (experience != null) {
+	  userDto.setJobTitleCompany(experience.getTitle() + " at " + experience.getCompany());
 	}
 
+	return userDto;
+  }
 
-	public UserDto toUserDto(User user, Integer role) {
-		UserDto userDto = new UserDto();
-		userDto.setEmail(user.getEmail());
-		userDto.setUserId(user.getId());
-		userDto.setName(user.getName());
-		userDto.setSurname(user.getSurname());
-		userDto.setDisplayName(user.getName() + ' ' + user.getSurname());
-		userDto.setUsername(user.getUsername());
-		userDto.setBirthdate(user.getBirthdate());
-		userDto.setAddress(user.getAddress());
-		userDto.setPhoneNumber(user.getPhoneNumber());
-		userDto.setImagePath(getUserPhotoFullUrl(user));
-		userDto.setDateCreated(user.getDateCreated());
-		userDto.setRole(role);
-		return userDto;
+
+  public UserSimpleDto toUserSimpleDto(Long userId) {
+
+	User user = userRepository.findById(userId).orElse(null);
+	UserSimpleDto userDto = new UserSimpleDto();
+	userDto.setDisplayName(user.getName() + ' ' + user.getSurname());
+	userDto.setUserId(user.getId());
+	userDto.setUsername(user.getUsername());
+	userDto.setImagePath(getUserPhotoFullUrl(user));
+
+	Experience experience = experienceRepository.findAllByUserIdOrderByStartDateDesc(userId).stream().findFirst().orElse(null);
+	if (experience != null) {
+	  userDto.setJobTitleCompany(experience.getTitle() + " at " + experience.getCompany());
 	}
+	return userDto;
+  }
 
+  public UserSimpleDto toUserSimpleDto(User user) {
+	UserSimpleDto userDto = new UserSimpleDto();
+	userDto.setDisplayName(user.getName() + ' ' + user.getSurname());
+	userDto.setUserId(user.getId());
+	userDto.setUsername(user.getUsername());
+	userDto.setImagePath(getUserPhotoFullUrl(user));
 
-	public UserSimpleDto toUserSimpleDto(Long userId) {
+	return userDto;
+  }
 
-		User user = userRepository.findById(userId).orElse(null);
-		UserSimpleDto userDto = new UserSimpleDto();
-		userDto.setDisplayName(user.getName() + ' ' + user.getSurname());
-		userDto.setUserId(user.getId());
-		userDto.setUsername(user.getUsername());
-		userDto.setImagePath(getUserPhotoFullUrl(user));
-
-		//experienceService.getUsersExperiences(userId).sort();
-		Experience experience = experienceRepository.findAllByUserIdOrderByStartDateDesc(userId).stream().findFirst().orElse(null);
-		if(experience != null ){
-		  userDto.setJobTitleCompany(experience.getTitle() + " at " +experience.getCompany()  );
-		}
-
-
-		return userDto;
+  /*
+   * Return the full path of users photo.
+   * If the photo doesn't exists returns empty String
+   */
+  public String getUserPhotoFullUrl(User user) {
+	if (user.getImgPath() != null) {
+	  return FileService.getFileFullUrl(user.getImgPath());
+	} else {
+	  return "";
 	}
-
-	public UserSimpleDto toUserSimpleDto(User user) {
-		UserSimpleDto userDto = new UserSimpleDto();
-		userDto.setDisplayName(user.getName() + ' ' + user.getSurname());
-		userDto.setUserId(user.getId());
-		userDto.setUsername(user.getUsername());
-		userDto.setImagePath(getUserPhotoFullUrl(user));
-
-		return userDto;
-	}
-
-	/*
-	 * Return the full path of users photo.
-	 * If the photo doesn't exists returns empty String
-	 */
-	public String getUserPhotoFullUrl(User user) {
-		if (user.getImgPath() != null) {
-			return FileService.getFileFullUrl(user.getImgPath());
-		} else {
-			return "";
-		}
-	}
+  }
 
 }
--- src/main/java/com/linkedin/dataentry/DatabaseCsvReader.java
+++ src/main/java/com/linkedin/dataentry/DatabaseCsvReader.java
@@ -24,135 +24,138 @@ import java.util.List;
  * The filename MUST be the same with the Entity (the first letter capital)
  * In order to save uses the repository that is created for each object
  * which MUST have the structure EntityRepository!
- * 
+ * <p>
  * The first line of csv file must be the field (first letter capital)
  * Warning it doesn't work if the are @ManyToOne or OneToMany etc.
- * 
+ * <p>
  * If can't be cast it returns NULL
  * If can't found setter the whole file fails and continue to the next one
  * If can't find repository fails and continue to the next file
  */
 @Component
 public class DatabaseCsvReader {
-	private final String[] FILE_ARRAY = new String[]{"database_files/Login.csv",
-			"database_files/Notification.csv",
-			"database_files/Job.csv", "database_files/Connection.csv",
-			"database_files/User.csv",
-			"database_files/Post.csv", "database_files/Comment.csv", "database_files/Like.csv",
-			"database_files/ConnectionRequest.csv",
-			"database_files/Message.csv"
-	};
-	private final String ENTITIES_PACKAGE_NAME = "com.linkedin.entities.database";
+  private final String[] FILE_ARRAY = new String[] {"database_files/Login.csv",
+	  "database_files/Notification.csv",
+	  "database_files/Job.csv", "database_files/Connection.csv",
+	  "database_files/User.csv",
+	  "database_files/Post.csv",
+	  "database_files/Comment.csv",
+	  "database_files/Like.csv",
+	  "database_files/ConnectionRequest.csv",
+	  "database_files/Message.csv",
+	  "database_files/Experience.csv"
+  };
+  private final String ENTITIES_PACKAGE_NAME = "com.linkedin.entities.database";
 
-	private final Repositories repositories;
+  private final Repositories repositories;
 
-	public static final SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+  public static final SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
 
-	public static final SimpleDateFormat dateTimeFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+  public static final SimpleDateFormat dateTimeFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
 
-	@Autowired
-	public DatabaseCsvReader(ListableBeanFactory listableBeanFactory) {
-		this.repositories = new Repositories(listableBeanFactory);
-	}
+  @Autowired
+  public DatabaseCsvReader(ListableBeanFactory listableBeanFactory) {
+	this.repositories = new Repositories(listableBeanFactory);
+  }
 
-	public void readFiles() {
-		for (String fileName : FILE_ARRAY) {
-			try {
-				FileReader fileReader = new FileReader(fileName);
-				String className = Paths.get(fileName).getFileName().toString().replace(".csv", "");
-				List<Object> objectsToCreate = readFile(fileReader, className);
-				JpaRepository repository = getObjectRepositoryFromFileName(className);
-				repository.saveAll(objectsToCreate);
-			} catch (Exception e) {
-				e.printStackTrace();
-				System.out.println("EDW SKAEI \n\n");
-			}
-		}
-	}
-
-	private List<Object> readFile(FileReader fileReader, String className) throws Exception {
-		Class<?> objClass = getClass(className);
-		CSVReader reader = new CSVReader(fileReader);
-
-		List<Method> methodList = createMethodList(reader, objClass);
-
-		List<Object> objectsToCreate = new ArrayList<>();
-		String[] line = reader.readNext();
-		while (line != null) {
-			Object obj = createAndSetFields(objClass, methodList, Arrays.asList(line));
-			objectsToCreate.add(obj);
-			line = reader.readNext();
-		}
-		return objectsToCreate;
-	}
-
-	private List<Method> createMethodList(CSVReader reader, Class<?> objClass) throws Exception {
-		List<String> headerColumnList = getHeaderColumnList(reader);
-		List<Method> methods = new ArrayList<>();
-		for (String headerName : headerColumnList) {
-			Method setterFromName = findSetterFromName(objClass, headerName);
-			methods.add(setterFromName);
-		}
-		return methods;
-	}
-
-	private JpaRepository getObjectRepositoryFromFileName(String className) throws Exception {
-		Class<?> objectClass = getClass(className);
-		return (JpaRepository) repositories.getRepositoryFor(objectClass).orElseThrow(() -> new Exception("repo not found"));
-	}
-
-	private Object createAndSetFields(Class<?> objClass, List<Method> methodList, List<String> valuesList) throws Exception {
-		Object instance = objClass.getConstructor().newInstance();
-		for (int i = 0; i < valuesList.size(); i++) {
-			Method method = methodList.get(i);
-			Object value = castValue(valuesList.get(i), method.getParameterTypes()[0]);
-			method.invoke(instance, value);
-		}
-		return instance;
-	}
-
-	private Object castValue(String valueToCast, Class<?> targetClass) {
-		try {
-			if (targetClass.equals(Integer.class)) {
-				return Integer.valueOf(valueToCast);
-			} else if (targetClass.equals(Long.class)) {
-				return Long.valueOf(valueToCast);
-			} else if (targetClass.equals(Boolean.class)) {
-				return Boolean.valueOf(valueToCast);
-			} else if (targetClass.equals(Date.class)) {
-				if (valueToCast.contains("T")) {
-					return dateTimeFormat.parse(valueToCast);
-				} else {
-					return dateFormat.parse(valueToCast);
-				}
-			} else if (targetClass.equals(String.class)) {
-				return valueToCast.substring(0, Integer.min(valueToCast.length(), 250));
-			} else if (targetClass.equals(Role.class)) {
-				return Role.valueOf(valueToCast);
-//				return Role.ROLE_USER;
-			} else {
-				return targetClass.cast(valueToCast);
-			}
-		} catch (ParseException e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
-
-	private Method findSetterFromName(Class<?> objClass, String fieldName) throws Exception {
-		return Arrays.stream(objClass.getMethods())
-				.filter((Method meth) -> meth.getName().equals("set" + fieldName))
-						.findFirst()
-				.orElseThrow(() -> new Exception("method for class not found " + objClass.getName()
-						+ "for field: " + fieldName));
-	}
-
-	private Class<?> getClass(String fileName) throws ClassNotFoundException {
+  public void readFiles() {
+	for (String fileName : FILE_ARRAY) {
+	  try {
+		FileReader fileReader = new FileReader(fileName);
 		String className = Paths.get(fileName).getFileName().toString().replace(".csv", "");
-		return Class.forName(ENTITIES_PACKAGE_NAME + "." + className);
+		List<Object> objectsToCreate = readFile(fileReader, className);
+		JpaRepository repository = getObjectRepositoryFromFileName(className);
+		repository.saveAll(objectsToCreate);
+	  } catch (Exception e) {
+		e.printStackTrace();
+		System.out.println("EDW SKAEI \n\n");
+	  }
 	}
+  }
 
-	private List<String> getHeaderColumnList(CSVReader reader) throws IOException {
-		return Arrays.asList(reader.readNext());
+  private List<Object> readFile(FileReader fileReader, String className) throws Exception {
+	Class<?> objClass = getClass(className);
+	CSVReader reader = new CSVReader(fileReader);
+
+	List<Method> methodList = createMethodList(reader, objClass);
+
+	List<Object> objectsToCreate = new ArrayList<>();
+	String[] line = reader.readNext();
+	while (line != null) {
+	  Object obj = createAndSetFields(objClass, methodList, Arrays.asList(line));
+	  objectsToCreate.add(obj);
+	  line = reader.readNext();
 	}
+	return objectsToCreate;
+  }
+
+  private List<Method> createMethodList(CSVReader reader, Class<?> objClass) throws Exception {
+	List<String> headerColumnList = getHeaderColumnList(reader);
+	List<Method> methods = new ArrayList<>();
+	for (String headerName : headerColumnList) {
+	  Method setterFromName = findSetterFromName(objClass, headerName);
+	  methods.add(setterFromName);
+	}
+	return methods;
+  }
+
+  private JpaRepository getObjectRepositoryFromFileName(String className) throws Exception {
+	Class<?> objectClass = getClass(className);
+	return (JpaRepository) repositories.getRepositoryFor(objectClass).orElseThrow(() -> new Exception("repo not found"));
+  }
+
+  private Object createAndSetFields(Class<?> objClass, List<Method> methodList, List<String> valuesList) throws Exception {
+	Object instance = objClass.getConstructor().newInstance();
+	for (int i = 0; i < valuesList.size(); i++) {
+	  Method method = methodList.get(i);
+	  Object value = castValue(valuesList.get(i), method.getParameterTypes()[0]);
+	  method.invoke(instance, value);
+	}
+	return instance;
+  }
+
+  private Object castValue(String valueToCast, Class<?> targetClass) {
+	try {
+	  if (targetClass.equals(Integer.class)) {
+		return Integer.valueOf(valueToCast);
+	  } else if (targetClass.equals(Long.class)) {
+		return Long.valueOf(valueToCast);
+	  } else if (targetClass.equals(Boolean.class)) {
+		return Boolean.valueOf(valueToCast);
+	  } else if (targetClass.equals(Date.class)) {
+		if (valueToCast.contains("T")) {
+		  return dateTimeFormat.parse(valueToCast);
+		} else {
+		  return dateFormat.parse(valueToCast);
+		}
+	  } else if (targetClass.equals(String.class)) {
+		return valueToCast.substring(0, Integer.min(valueToCast.length(), 250));
+	  } else if (targetClass.equals(Role.class)) {
+		return Role.valueOf(valueToCast);
+//				return Role.ROLE_USER;
+	  } else {
+		return targetClass.cast(valueToCast);
+	  }
+	} catch (ParseException e) {
+	  e.printStackTrace();
+	  return null;
+	}
+  }
+
+  private Method findSetterFromName(Class<?> objClass, String fieldName) throws Exception {
+	return Arrays.stream(objClass.getMethods())
+		.filter((Method meth) -> meth.getName().equals("set" + fieldName))
+		.findFirst()
+		.orElseThrow(() -> new Exception("method for class not found " + objClass.getName()
+			+ "for field: " + fieldName));
+  }
+
+  private Class<?> getClass(String fileName) throws ClassNotFoundException {
+	String className = Paths.get(fileName).getFileName().toString().replace(".csv", "");
+	return Class.forName(ENTITIES_PACKAGE_NAME + "." + className);
+  }
+
+  private List<String> getHeaderColumnList(CSVReader reader) throws IOException {
+	return Arrays.asList(reader.readNext());
+  }
 }
